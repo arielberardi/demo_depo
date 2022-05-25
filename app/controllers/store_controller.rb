0738b086771c617bd3d320407f4ccc765d3927a2
develop <arielberardi95@gmail.com>
2022-05-25 13:24:28 -0300

    Add Store Index page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,6 @@
 class StoreController < ApplicationController
   def index
+    @page_title = 'The Book store'
     @products = Product.order(:title)
   end
 end
--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -1,8 +1,14 @@
 require "test_helper"
 
 class StoreControllerTest < ActionDispatch::IntegrationTest
+  fixtures :products
+
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'nav.side_nav a', minimum: 4
+    assert_select 'main ul.catalog li', 3
+    assert_select 'h2', products(:one).title
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 end
